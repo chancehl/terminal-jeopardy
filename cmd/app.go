904f46788f7979d48f1bb2da2e02c5cf8d080793
Chance Linz <clinz.dev@gmail.com>
2025-02-15 22:23:30 -0800

    setting project up for bubbletea

--- cmd/app.go
+++ cmd/app.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	internal_tea "github.com/chancehl/terminal-jeopardy/internal/tea"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	fmt.Println("hello world")
+	p := tea.NewProgram(internal_tea.InitialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("an error happened: %v", err)
+		os.Exit(1)
+	}
 }
---
+++ internal/tea/tea.go
@@ -0,0 +1,80 @@
+package internal_tea
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func InitialModel() model {
+	return model{
+		choices:  []string{"hello", "world"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		key := msg.String()
+		switch key {
+		case "q":
+			return m, tea.Quit
+		case "up":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		case "enter":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "What should we buy at the market?\n\n"
+
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
+}
