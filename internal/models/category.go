a71bb78fb3f5451fecef830d8f42eba6f7c11e54
Chance Linz <clinz.dev@gmail.com>
2025-02-22 08:00:51 -0800

    removing unused methods in category.go

--- internal/models/category.go
+++ internal/models/category.go
@@ -1,12 +1,6 @@
 package models
 
-import "fmt"
-
 type JeopardyCategory struct {
 	Name      string             `json:"name"`
 	Questions []JeopardyQuestion `json:"questions"`
-}
-
-func (c *JeopardyCategory) Print() {
-	fmt.Println(c.Name)
 }
